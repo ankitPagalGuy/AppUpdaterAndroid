64254a9348438eee8f825a3543a9edcda937aedc
ankitPagalGuy <ankit.choudhary@pagalguy.com>
2016-10-04 10:58:02 +0530

    created Splash Activity to check kill Switch here

---
+++ app/src/main/java/com/appupdaterandroid/SplashActivity.java
@@ -0,0 +1,10 @@
+package com.appupdaterandroid;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by techteam on 04/10/16.
+ */
+
+public class SplashActivity  extends AppCompatActivity{
+}
