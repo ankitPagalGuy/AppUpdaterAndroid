5736aabb388079f704d966813829007a677eb8bb
ankitPagalGuy <ankit.choudhary@pagalguy.com>
2016-10-07 14:26:19 +0530

    added method to open in playstore

--- app/src/main/java/com/appupdaterandroid/SplashActivity.java
+++ app/src/main/java/com/appupdaterandroid/SplashActivity.java
@@ -2,6 +2,7 @@ package com.appupdaterandroid;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -142,7 +143,7 @@ public class SplashActivity  extends AppCompatActivity{
     alertDialogBuilder.setPositiveButton("Update", new DialogInterface.OnClickListener() {
       @Override
       public void onClick(DialogInterface arg0, int arg1) {
-        Toast.makeText(SplashActivity.this,"You clicked yes button", Toast.LENGTH_LONG).show();
+        openPlayStore();
       }
     });
 
@@ -169,4 +170,14 @@ public class SplashActivity  extends AppCompatActivity{
     finish();
   }
 
+
+  public void openPlayStore(){
+
+    final String appPackageName = BuildConfig.APPLICATION_ID; // getPackageName() from Context or Activity object
+    try {
+      startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+    } catch (android.content.ActivityNotFoundException anfe) {
+      startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
+    }
+  }
 }
